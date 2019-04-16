e5ba10d159b0c171cacc02c5e7f793ed9871e858
aisisjulian <ajulia5@uic.edu>
2019-04-16 18:50:38 -0500

    hangman

--- src/hangman/ServerFx.java
+++ src/hangman/ServerFx.java
@@ -1,4 +1,19 @@
 package hangman;
 
-public class ServerFx {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ServerFx extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
