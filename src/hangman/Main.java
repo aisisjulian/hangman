1b306b10bf18b039e66d8f41670840e843b7f65e
aisisjulian <ajulia5@uic.edu>
2019-04-16 16:35:53 -0500

    hangman

---
+++ src/hangman/Main.java
@@ -0,0 +1,19 @@
+package hangman;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module Hangman {
+    requires javafx.fxml;
+    requires javafx.controls;
+    opens hangman;
+}
