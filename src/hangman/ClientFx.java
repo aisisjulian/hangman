40aadef579c3eb1240ddc68bf226a6df8377b1ce
aisisjulian <ajulia5@uic.edu>
2019-04-17 18:33:31 -0500

    more client GUI stuff

--- src/hangman/ClientFx.java
+++ src/hangman/ClientFx.java
@@ -2,16 +2,18 @@ package hangman;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -27,11 +29,13 @@ public class ClientFx extends Application {
     boolean started = false;
 
 
-    private Scene startScene, gameScene, endScene;
+    //private Scene startScene, gameScene, endScene;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Hello World");
+        StartScene ss = new StartScene();
+        primaryStage.setScene(ss.scene);
         primaryStage.show();
     }
 
@@ -49,27 +53,59 @@ public class ClientFx extends Application {
         Image startBackgroundImage;
         Background startBackground;
         Button singlePlayerButton, multiPlayerButton, twoPlayerButton, threePlayerButton, fourPlayerButton;
+        VBox connectionBox;
+        VBox optionsBox;
         Button connect, start;
         TextArea ipInput, portInput;
         Label header, ipLabel, portLabel, numPlayersLabel;
 
         public StartScene(){
             startPane = new BorderPane();
-            startBackgroundImage  = new Image("startScene.jpg");
+            startBackgroundImage  = new Image("startSceneOpt.png");
             startBackground = new Background(new BackgroundFill(new ImagePattern(startBackgroundImage), CornerRadii.EMPTY, Insets.EMPTY));
+            Image buttonBackgroundImage  = new Image("buttonBackground.png");
+            Background buttonBackground = new Background(new BackgroundFill(new ImagePattern(buttonBackgroundImage), CornerRadii.EMPTY, Insets.EMPTY));
             startPane.setBackground(startBackground);
-
             singlePlayerButton = new Button(" Single-Player");
             multiPlayerButton = new Button("Multi-Player");
             twoPlayerButton = new Button("2-Player");
             threePlayerButton = new Button( "3-Player");
             fourPlayerButton = new Button("4-Player");
 
-            header = new Label("Welcome :-) Connect to Server....");
+            header = new Label("");
+            header.setTextFill(Color.WHITE);
+            header.setPrefSize(300, 40);
+            header.setAlignment(Pos.CENTER);
+            portLabel = new Label("Port : ");
+            portLabel.setTextFill(Color.WHITE);
+            portLabel.setPrefSize(50, 20);
+            portLabel.setTextAlignment(TextAlignment.CENTER);
+            portInput = new TextArea();
+            portInput.setPrefSize(150, 20);
 
+            ipLabel = new Label("IP: ");
+            ipLabel.setTextFill(Color.WHITE);
+            ipLabel.setPrefSize(50, 20);
+            ipLabel.setTextAlignment(TextAlignment.CENTER);
+            ipInput = new TextArea();
+            ipInput.setPrefSize(150, 20);
 
+            HBox ipBox = new HBox(ipLabel, ipInput);
+            HBox portBox = new HBox(portLabel, portInput);
+            ipBox.setAlignment(Pos.CENTER);
+            portBox.setAlignment(Pos.CENTER);
 
+            connect = new Button("CONNECT");
+            connect.setPrefSize(250, 80);
+            connect.setBackground(buttonBackground);
+            connect.setTextFill(Color.NAVY);
+            connect.setFont(Font.font("Courier", FontWeight.EXTRA_BOLD, 22));
+            connect.setTextAlignment(TextAlignment.CENTER);
+            optionsBox = new VBox(10, header, ipBox, portBox, connect);
+            optionsBox.setAlignment(Pos.CENTER);
+            startPane.setCenter(optionsBox);
 
+            scene = new Scene(startPane, 500, 500);
         }
     }
 
